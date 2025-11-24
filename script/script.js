0560a33a0b0093de5df44141ff7659a25eab5ffd
gabriellysaless008 <gabrielly.sales.silva@escola.pr.gov.br>
2025-11-24 11:27:58 -0300

    primeiro commit

---
+++ script/script.js
@@ -0,0 +1,11 @@
+let vet = []
+
+function adicionar(){
+    vet [0] = document.getElementById("primeiro").value;
+    vet [1] = document.getElementById("primeiro").value;
+    vet [2] = document.getElementById("primeiro").value;
+}
+
+function imprimir(){
+    console.log (vet);
+} 
