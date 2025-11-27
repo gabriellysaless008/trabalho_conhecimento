eb2e7b82424ccea9bde0e1d4174a6e229e012b75
gabriellysaless008 <gabrielly.sales.silva@escola.pr.gov.br>
2025-11-27 12:18:56 -0300

    segundo commit

--- script/script.js
+++ script/script.js
@@ -1,11 +1,18 @@
-let vet = []
+let vet = [];
 
 function adicionar(){
-    vet [0] = document.getElementById("primeiro").value;
-    vet [1] = document.getElementById("primeiro").value;
-    vet [2] = document.getElementById("primeiro").value;
+    vet[0] = document.getElementById("primeiro").value;
+    document.getElementById("primeiro").textContent = "";
+    console.log (vet[0]);
+
 }
 
 function imprimir(){
-    console.log (vet);
-} 
+    console.log("imprimir");
+    document.getElementById("mensagem").textContent = vet[0];
+}
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
