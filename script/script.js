fe3ae96a7f3a5e6bb6544ec0d6a7f223d12b5b13
gabriellysaless008 <gabrielly.sales.silva@escola.pr.gov.br>
2025-12-01 08:13:54 -0300

    começando novamente

--- script/script.js
+++ script/script.js
@@ -1,18 +1,45 @@
-let vet = [];
-
-function adicionar(){
-    vet[0] = document.getElementById("primeiro").value;
-    document.getElementById("primeiro").textContent = "";
-    console.log (vet[0]);
-
+function adicionarNaLista(){
+    console.log ("batatinha");
 }
 
-function imprimir(){
-    console.log("imprimir");
-    document.getElementById("mensagem").textContent = vet[0];
-}
 
-for (let index = 0; index < array.length; index++) {
-    const element = array[index];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let vet = [];
+
+// function adicionar(){
+//     vet[0] = document.getElementById("primeiro").value;
+//     document.getElementById("primeiro").textContent = "";
+//     console.log (vet[0]);
+
+// }
+
+// function imprimir(){
+//     console.log("imprimir");
+//     document.getElementById("mensagem").textContent = vet[0];
+// }
+
+//for (let index = 0; index < array.length; index++) {
+    //const element = array[index];
     
-}
+//}
